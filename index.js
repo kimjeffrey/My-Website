4a3e16fd66f32eff9ea05979665a3900a8766044
Jeffrey Kim <jeffreyk93@gmail.com>
2021-01-05 16:59:53 -0500

    Changed home design

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+  const navBar = document.querySelector(".navbar");
+  const navLinks = document.querySelectorAll(".nav-link");
+  const burger = document.querySelector(".burger");
+
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    navBar.style.padding = "1.5rem 15%";
+    navBar.style.backgroundColor = "#343a40";
+    navLinks.forEach(navLink => {
+      navLink.style.color = "white";
+      navLink.style.fontSize = "25px"
+    });
+    burger.style.color = "white";
+  } else {
+    navBar.style.padding = "3rem 15%";
+    navBar.style.backgroundColor = "transparent";
+    navLinks.forEach(navLink => {
+      navLink.style.color = "black";
+      navLink.style.fontSize = "35px";
+    });
+    burger.style.color = "black";
+  }
+}
+
 function expandNav(){
   let display = document.querySelector(".nav-expanded");
   display.classList.toggle('show');
